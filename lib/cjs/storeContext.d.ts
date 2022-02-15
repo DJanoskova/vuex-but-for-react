03cce5adc08ccb17fd93899f13be3a7ba86c6205
Dana Janoskova <dana@moonpay.com>
2022-02-15 01:10:17 +0100

    dont include lib in git repo

--- lib/cjs/storeContext.d.ts
+++
@@ -1,8 +0,0 @@
-import { Context } from 'react';
-import { ActionParamsType } from './types';
-export declare const mutationsContext: Context<Record<string, (...args: any) => void>>;
-export declare const MutationsProvider: import("react").MemoExoticComponent<import("react").Provider<Record<string, (...args: any) => void>>>;
-export declare const actionsContext: Context<Record<string, (context: ActionParamsType, args?: any) => Promise<any>>>;
-export declare const ActionsProvider: import("react").MemoExoticComponent<import("react").Provider<Record<string, (context: ActionParamsType, args?: any) => Promise<any>>>>;
-export declare const gettersContext: Context<Record<string, Context<any>>>;
-export declare const GettersProvider: import("react").MemoExoticComponent<import("react").Provider<Record<string, Context<any>>>>;
